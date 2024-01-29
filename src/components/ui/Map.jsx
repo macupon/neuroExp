321f834c21ae98911d0cc72d1960e7b072989d94
macupon <macupon@gmail.com>
2024-01-29 14:15:29 +0100

    margins

--- src/components/ui/Map.jsx
+++ src/components/ui/Map.jsx
@@ -4,8 +4,8 @@ export default function Map() {
   return (
     <>
     <div className="flex items-center flex-col md:flex-row justify-center">
-        <div className="mx-16">
-            <h3 className="text-xl mb-4"><b>Contact Us</b></h3>
+        <div className="mx-5 lg:mx-16">
+            <h3 className="text-xl my-4"><b>Contact Us</b></h3>
             <ul className="text-lg leading-8">
                 <li>
                     Address: Hørkær 22 
@@ -24,8 +24,8 @@ export default function Map() {
                 </li>
             </ul>
         </div>
-        <div className="">
-            <iframe className=" rounded-xl  m-24" width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=H%C3%B8rk%C3%A6r%2022,%20Herlev%202730,%20Danmark+(NeuroExp)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/population/">Population Estimator map</a></iframe>
+        <div>
+            <iframe className="rounded-xl my-6 lg:m-24" width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=H%C3%B8rk%C3%A6r%2022,%20Herlev%202730,%20Danmark+(NeuroExp)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/population/">Population Estimator map</a></iframe>
         </div>
     </div>
     </>
