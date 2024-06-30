4f5dad3b10a0864567fab73a560498135130eed1
macupon <macupon@gmail.com>
2024-06-30 19:42:25 +0200

    removed form temporarily

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -4,10 +4,10 @@ import Map from "../components/ui/Map";
 export default function ContactUs() {
   return (
     <div className="flex flex-col md:flex-col p-4 md:p-8 rounded-xl justify-center  md:leading-10">
-      <div className="bg-white/50 p-4 md:p-8 mt-8 rounded-xl w-[100%]">
+      {/* <div className="bg-white/50 p-4 md:p-8 mt-8 rounded-xl shadow-md w-[100%]">
         <ProfileForm />
-      </div>
-      <div className="bg-white/50 rounded-xl shadow-md mx-5  my-8">
+      </div> */}
+      <div className="bg-white/50 rounded-xl shadow-md mx-5 my-8 w-[100%]">
         <Map />
       </div>
     </div>
